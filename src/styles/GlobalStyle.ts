c4512d970aff76a169cb094724e43dcbe644191d
jinhwansuh <nick02190219@gmail.com>
2022-12-19 17:48:06 +0900

    design: global style 변경

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,10 +1,24 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
+  * {
     box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+  }
+  body {
+    max-width: 700px;
+    margin: 0 auto;
+    overflow-x: hidden;
+  }
+  #__next {
+    background-color: #eee;
+    min-height: 100vh;
+    width: 100%;
+  }
+  a {
+    color: inherit;
+    text-decoration: none;
   }
 `;
 
