8850722a4e23dcfa8a5332225efbf8f9c5fd9851
jinhwansuh <nick02190219@gmail.com>
2022-12-21 00:33:49 +0900

    feat: 부드러운 motion을 위해 key와 mode 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,13 +4,13 @@ import { ThemeProvider } from 'styled-components';
 import GlobalStyle from '~/styles/GlobalStyle';
 import { theme } from '~/styles/theme';
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps, router }: AppProps) {
   return (
     <>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
-        <AnimatePresence initial={false}>
-          <Component {...pageProps} />
+        <AnimatePresence mode='wait' initial={false}>
+          <Component {...pageProps} key={router.route} />
         </AnimatePresence>
       </ThemeProvider>
     </>
