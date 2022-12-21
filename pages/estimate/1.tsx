bc84c9cc50da9230f277c259024b555b50ed5880
jinhwansuh <nick02190219@gmail.com>
2022-12-21 19:29:57 +0900

    feat: input 컴포넌트 적용

--- pages/estimate/1.tsx
+++ pages/estimate/1.tsx
@@ -1,5 +1,55 @@
+import { useRouter } from 'next/router';
+import { useSetRecoilState } from 'recoil';
+import styled from 'styled-components';
+import { EstimateInput } from '~/components';
+import Header from '~/components/Header';
+import { researchIndexState } from '~/atoms/research';
+import Transitions from '~/layout/transitions';
+
 const EstimateFirstPage = () => {
-  return <div>첫페이지</div>;
+  const router = useRouter();
+  const setResearchIndex = useSetRecoilState(researchIndexState);
+
+  const handleNextClick = () => {
+    setResearchIndex((prev) => ({ ...prev, first: true }));
+    router.push('/estimate/2');
+  };
+  const handleInputChange = () => {
+    console.log(123);
+  };
+  return (
+    <>
+      <Header />
+      <Transitions>
+        <InputWrapper>
+          <EstimateInput
+            handleInputChange={handleInputChange}
+            title={'보유 현금'}
+            tag={'만원'}
+          />
+          <EstimateInput
+            handleInputChange={handleInputChange}
+            title={'한달 저축 가능 금액'}
+            tag={'만원'}
+          />
+          <EstimateInput
+            handleInputChange={handleInputChange}
+            title={'가가가'}
+            tag={'만원'}
+          />
+        </InputWrapper>
+
+        <button onClick={handleNextClick}>다음으로</button>
+      </Transitions>
+    </>
+  );
 };
+
+const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default EstimateFirstPage;
