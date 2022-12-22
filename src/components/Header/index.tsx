3a7cf775db3585c07036f8d6424e35127c664d15
jinhwansuh <nick02190219@gmail.com>
2022-12-23 03:07:03 +0900

    design: header 컴포넌트 디자인 변경

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,33 @@
+import { useRouter } from 'next/router';
 import styled from 'styled-components';
 
 const Header = () => {
-  return <Container>헤더</Container>;
+  const router = useRouter();
+
+  const handleHomeClick = () => {
+    router.push('/');
+  };
+
+  return (
+    <Container>
+      <HeaderText>{'<'}</HeaderText>
+      <HeaderText onClick={handleHomeClick}>집사요</HeaderText>
+      <HeaderText>블라블라</HeaderText>
+    </Container>
+  );
 };
 
 const Container = styled.header`
   z-index: 9000;
   height: 50px;
   background-color: #b6b6ef;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const HeaderText = styled.div`
+  font-size: 20px;
 `;
 
 export default Header;
