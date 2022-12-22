95bd5a321305f39f3ef0f0167e8de86e0f280421
jinhwansuh <nick02190219@gmail.com>
2022-12-22 18:10:33 +0900

    feat: 첫 입력 페이지 input과 recoil 연결

--- pages/estimate/1.tsx
+++ pages/estimate/1.tsx
@@ -1,46 +1,63 @@
 import { useRouter } from 'next/router';
-import { useSetRecoilState } from 'recoil';
+import { ChangeEvent, useState } from 'react';
+import { useRecoilState, useSetRecoilState } from 'recoil';
 import styled from 'styled-components';
-import { EstimateInput } from '~/components';
+import { Button, EstimateInput } from '~/components';
 import Header from '~/components/Header';
-import { researchIndexState } from '~/atoms/research';
-import Transitions from '~/layout/transitions';
+import { researchFirstState, researchIndexState } from '~/atoms/research';
 
 const EstimateFirstPage = () => {
   const router = useRouter();
   const setResearchIndex = useSetRecoilState(researchIndexState);
+  const [researchState, setResearchState] = useRecoilState(researchFirstState);
+  const [isError, setIsError] = useState(false);
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setResearchState((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
 
   const handleNextClick = () => {
-    setResearchIndex((prev) => ({ ...prev, first: true }));
-    router.push('/estimate/2');
+    if (researchState.cash && researchState.rate && researchState.saving) {
+      setIsError(() => false);
+      setResearchIndex((prev) => ({ ...prev, first: true }));
+      router.push('/estimate/2');
+    } else {
+      setIsError(() => true);
+    }
   };
-  const handleInputChange = () => {
-    console.log(123);
-  };
+
   return (
     <>
-      <Header />
-      <Transitions>
+      <main>
+        <Header />
         <InputWrapper>
           <EstimateInput
             handleInputChange={handleInputChange}
             title={'보유 현금'}
             tag={'만원'}
+            name={'cash'}
+            type={'number'}
           />
           <EstimateInput
             handleInputChange={handleInputChange}
             title={'한달 저축 가능 금액'}
             tag={'만원'}
+            name={'saving'}
+            type={'number'}
           />
           <EstimateInput
             handleInputChange={handleInputChange}
-            title={'가가가'}
+            title={'인상률'}
             tag={'만원'}
+            name={'rate'}
+            type={'number'}
           />
         </InputWrapper>
 
-        <button onClick={handleNextClick}>다음으로</button>
-      </Transitions>
+        {isError && <ErrorMessage>모든 항목을 입력해주세요</ErrorMessage>}
+
+        <Button content='다음으로' handleButtonClick={handleNextClick} />
+      </main>
     </>
   );
 };
@@ -50,6 +67,10 @@ const InputWrapper = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+`;
+
+const ErrorMessage = styled.div`
+  color: red;
 `;
 
 export default EstimateFirstPage;
