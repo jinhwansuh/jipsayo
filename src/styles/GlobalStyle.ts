bb2eb6e825d82e80544f5950231d49109539058c
jinhwansuh <nick02190219@gmail.com>
2022-12-22 17:38:45 +0900

    design: global styled 변경

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -6,19 +6,26 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
     margin: 0;
   }
-  body {
+  html, body {
     max-width: 700px;
     margin: 0 auto;
     overflow-x: hidden;
+    height: 100%;
   }
   #__next {
     background-color: #eee;
     min-height: 100vh;
     width: 100%;
+    height: 100%;
   }
   a {
     color: inherit;
     text-decoration: none;
+  }
+  main {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
   }
 `;
 
