d957f301f02687dffc503f49413ad061ce8f0248
jinhwansuh <nick02190219@gmail.com>
2022-12-24 04:15:17 +0900

    feat: page의 route값을 상수로 관리

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,3 +4,10 @@ export const API_METHOD = {
   PUT: 'put',
   DELETE: 'delete',
 } as const;
+
+export const PAGE_ROUTE = {
+  HOME: '/',
+  RESEARCH_FIRST: '/research/1',
+  RESEARCH_SECOND: '/research/2',
+  RESULT: '/result',
+} as const;
