26c2a23dc1879fa45e5ccdf8d28e2fd9f5b9948e
jinhwansuh <nick02190219@gmail.com>
2022-12-26 18:54:36 +0900

    rename: 컴포넌트 파일 common domains 분리

--- src/components/domains/index.ts
+++ src/components/domains/index.ts
@@ -1,4 +1,2 @@
-export { default as Header } from './Header';
-export { default as NextHead } from './NextHead';
 export { default as EstimateInput } from './EstimateInput';
 export { default as Button } from './Button';
