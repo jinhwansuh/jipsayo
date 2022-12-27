32d51cc8c51347383a273d886e2fbd79b4cb2b78
jinhwansuh <nick02190219@gmail.com>
2022-12-27 21:10:40 +0900

    refactor: api 기본 설정 변경

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,15 +1,19 @@
-import axios, { AxiosRequestConfig } from 'axios';
+import axios from 'axios';
 
 const API_END_POINT = process.env.NEXT_PUBLIC_API_END_POINT;
 
-const axiosInstance = axios.create({
-  baseURL: API_END_POINT,
-});
+const axiosInstance = axios.create();
 
+axiosInstance.defaults.baseURL = API_END_POINT;
 axiosInstance.defaults.headers.post['Content-Type'] = 'application/json';
+axiosInstance.interceptors.response.use(
+  (res) => {
+    return res;
+  },
+  (error) => {
+    console.error(error);
+    return Promise.reject(error);
+  },
+);
 
-const request = (config: AxiosRequestConfig) => {
-  return axiosInstance(config);
-};
-
-export default request;
+export default axiosInstance;
