ca35a87a2daf9918514edc36593b5c1d15802f52
jinhwansuh <nick02190219@gmail.com>
2022-12-28 20:40:16 +0900

    feat: house mock data 추가

--- src/utils/house.ts
+++ src/utils/house.ts
@@ -14,3 +14,17 @@ export const initialResearch = {
   saving: '',
   rate: '',
 };
+
+export const mockHouseData = {
+  id: 1,
+  jibunAddress: '충남 천안시 서북구 성정동 1438',
+  roadAddress: '충남 천안시 서북구 성정공원3길 4',
+  cost: 5500,
+  hangCode: 4413310200,
+  danjiName: '학산리젠다빌3차',
+  postCode: 31110,
+  latitude: 36.826169496942,
+  longitude: 127.141328400903,
+  createdDate: '2022-12-28T18:04:31',
+  modifiedDate: '2022-12-28T18:04:31',
+};
