6fdd7a7202f52a4c4f895aa5e581ef8f8dcbf9c1
jinhwansuh <nick02190219@gmail.com>
2022-12-28 20:38:09 +0900

    fix: proxy 설정으로 base url 제거

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,8 @@
 import axios from 'axios';
 
-const API_END_POINT = process.env.NEXT_PUBLIC_API_END_POINT;
-
 const axiosInstance = axios.create();
 
-axiosInstance.defaults.baseURL = API_END_POINT;
-axiosInstance.defaults.headers.post['Content-Type'] = 'application/json';
+axiosInstance.defaults.headers['Content-Type'] = 'application/json';
 axiosInstance.interceptors.response.use(
   (res) => {
     return res;
