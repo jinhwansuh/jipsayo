ec8f7b8761fa16df3f6681460dc45d181b6837fb
jinhwansuh <nick02190219@gmail.com>
2022-12-29 03:03:58 +0900

    fix: response data 형식에 맞도록 설정

--- src/api/house.ts
+++ src/api/house.ts
@@ -2,7 +2,7 @@ import { FetchHouseRequest, FetchHouseResponse } from '~/types/house';
 import axiosInstance from '.';
 
 export const getHouse = (data: FetchHouseRequest) => {
-  return axiosInstance.get<FetchHouseResponse>('/api/v1/houses', {
+  return axiosInstance.get<{ data: FetchHouseResponse }>('/api/v1/houses', {
     params: {
       jibunAddress: data.jibunAddress,
     },
