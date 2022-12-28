3b815f0f91e91b0cc142838dddcb4b8922abb3e1
jinhwansuh <nick02190219@gmail.com>
2022-12-28 22:55:58 +0900

    refactor: dynamic import 적용

--- pages/research/2.tsx
+++ pages/research/2.tsx
@@ -1,206 +1,55 @@
+import dynamic from 'next/dynamic';
 import { useRouter } from 'next/router';
 import Script from 'next/script';
-import { useEffect, useRef, useState } from 'react';
-import { motion } from 'framer-motion';
+import { useEffect, useState } from 'react';
 import { useRecoilState } from 'recoil';
-import styled from 'styled-components';
-import Transitions from '~/layouts/Transitions';
 import { NextHead } from '~/components/common';
-import { SearchAddressData, SearchResize } from '~/types/research';
-import { initialAddress } from '~/utils/house';
-import { researchState } from '~/atoms/research';
+import { researchIndexState } from '~/atoms/research';
 import { PAGE_ROUTE } from '~/constants';
+
+const DynamicResearch2 = dynamic(
+  () => import('~/components/dynamicPage/DynamicResearch2'),
+  {
+    ssr: false,
+  },
+);
 
 const ResearchSecondPage = () => {
   const router = useRouter();
-  const searchFrameRef = useRef<HTMLDivElement>(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-  const searchFrame = searchFrameRef.current as HTMLDivElement;
-  const [addressState, setAddressState] =
-    useState<typeof initialAddress>(initialAddress);
-  const [isComplete, setIsComplete] = useState(false);
-  const [researchRecoilState, setResearchRecoilState] =
-    useRecoilState(researchState);
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasData, setHasData] = useState(false);
+  const [pageRecoilState, setPageRecoilState] =
+    useRecoilState(researchIndexState);
 
   useEffect(() => {
-    // ref를 위해 적용
-    setIsLoading(false);
+    setIsLoading(true);
+
+    if (pageRecoilState.first) {
+      setHasData(true);
+      setIsLoading(() => false);
+      return;
+    }
+
+    const timer = setTimeout(() => {
+      router.replace(PAGE_ROUTE.RESEARCH_FIRST, undefined, { shallow: true });
+    }, 4000);
+
+    setIsLoading(() => false);
+    return () => {
+      clearTimeout(timer);
+    };
   }, []);
 
-  const frameCloseClick = () => {
-    setIsOpen(false);
-  };
-
-  const frameOpenClick = () => {
-    setAddressState(() => initialAddress);
-    setIsComplete(() => false);
-    new daum.Postcode({
-      oncomplete: function (data: SearchAddressData) {
-        const {
-          userSelectedType,
-          roadAddress,
-          jibunAddress,
-          bname,
-          buildingName,
-          apartment,
-          zonecode,
-        } = data;
-
-        let extraAddr = '';
-
-        if (userSelectedType === 'R') {
-          if (bname !== '' && /[동|로|가]$/g.test(bname)) {
-            extraAddr += bname;
-          }
-          if (buildingName !== '' && apartment === 'Y') {
-            extraAddr += extraAddr !== '' ? ', ' + buildingName : buildingName;
-          }
-          if (extraAddr !== '') {
-            extraAddr = ' (' + extraAddr + ')';
-          }
-
-          setAddressState((prev) => ({
-            ...prev,
-            roadAddress,
-            zonecode,
-            userSelectedType,
-            extraAddr,
-          }));
-        } else {
-          setAddressState((prev) => ({
-            ...prev,
-            userSelectedType,
-            jibunAddress,
-            zonecode,
-          }));
-        }
-
-        setIsOpen(() => false);
-        setIsComplete(() => true);
-      },
-
-      onresize: function (size: SearchResize) {
-        searchFrame.style.height = size.height + 'px';
-      },
-      width: '100%',
-      height: '100%',
-    }).embed(searchFrame);
-
-    setIsOpen(() => true);
-  };
-
-  const handleSubmitClick = () => {
-    if (addressState.userSelectedType) {
-      setResearchRecoilState((prev) => ({ ...prev, ...addressState }));
-      router.push(PAGE_ROUTE.RESULT);
-    }
-  };
+  if (!hasData && !isLoading) return <div>데이터가 없습니다</div>;
 
   return (
     <>
       <NextHead title='주소 입력' />
       <Script src='//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js' />
 
-      <Transitions>
-        <StyledAddressWrapper onClick={frameOpenClick}>
-          {addressState.userSelectedType === '' ? (
-            <StyledSelectText>눌러서 아파트 검색!</StyledSelectText>
-          ) : addressState.userSelectedType === 'R' ? (
-            <>
-              <div>{addressState.roadAddress}</div>
-              <div>{addressState.extraAddr}</div>
-            </>
-          ) : (
-            <div>{addressState.jibunAddress}</div>
-          )}
-        </StyledAddressWrapper>
-
-        <StyledFrameContainer>
-          <StyledFrameWrapper
-            ref={searchFrameRef}
-            style={{ display: isOpen ? 'block' : 'none' }}
-          >
-            <StyledExitImg
-              src='//t1.daumcdn.net/postcode/resource/images/close.png'
-              id='btnFoldWrap'
-              onClick={frameCloseClick}
-              alt='접기 버튼'
-            />
-          </StyledFrameWrapper>
-        </StyledFrameContainer>
-        <div>
-          {isComplete && (
-            <StyleButtonWrapper>
-              <StyledMotionButton
-                animate={{ scale: [1, 1.5, 1.2, 1] }}
-                transition={{
-                  ease: 'easeInOut',
-                  repeat: Infinity,
-                  duration: 2,
-                }}
-                onClick={handleSubmitClick}
-              >
-                다음으로
-              </StyledMotionButton>
-            </StyleButtonWrapper>
-          )}
-        </div>
-      </Transitions>
+      {hasData && <DynamicResearch2 />}
     </>
   );
 };
-
-const StyledAddressWrapper = styled.div`
-  height: 50px;
-  background-color: #ddd;
-  cursor: pointer;
-  &:hover {
-    background-color: #ccc;
-  }
-`;
-
-const StyledSelectText = styled.div`
-  text-align: center;
-  font-size: 23px;
-  height: 100%;
-`;
-
-const StyledFrameContainer = styled.div``;
-
-const StyledFrameWrapper = styled.div`
-  display: none;
-  border: 1px solid;
-  width: 500px;
-  height: 466px;
-  margin: 5px 0;
-  position: relative;
-  min-height: 400px;
-  max-height: 500px;
-  @media screen and (max-width: 400px) {
-    width: 90%;
-  }
-`;
-
-const StyledExitImg = styled.img`
-  cursor: pointer;
-  position: absolute;
-  right: 0px;
-  top: -1px;
-  z-index: 1;
-`;
-
-const StyleButtonWrapper = styled.div`
-  display: flex;
-  height: 200px;
-  justify-content: center;
-  align-items: center;
-`;
-
-const StyledMotionButton = styled(motion.button)`
-  width: 60%;
-  height: 50px;
-  cursor: pointer;
-`;
 
 export default ResearchSecondPage;
