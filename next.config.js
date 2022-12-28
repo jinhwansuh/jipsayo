bab3eb21ad0f04d9f527f456c4a38d7e69d6a10c
jinhwansuh <nick02190219@gmail.com>
2022-12-28 20:03:04 +0900

    config: next js CORS 에러 proxy 설정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_END_POINT}/:path*`,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
