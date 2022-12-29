3c49587c95f23afa79c0152e9e841bb79fd23cd0
jinhwansuh <nick02190219@gmail.com>
2022-12-29 16:58:26 +0900

    feat: research 데이터를 서버에 저장하는 api 구현

--- src/api/research.ts
+++ src/api/research.ts
@@ -2,8 +2,5 @@ import { PostResearchRequest, PostResearchResponse } from '~/types/research';
 import axiosInstance from '.';
 
 export const postResearch = (data: PostResearchRequest) => {
-  // 수정 필요
-  return axiosInstance.post<PostResearchResponse>('url', {
-    data,
-  });
+  return axiosInstance.post<PostResearchResponse>('/api/v1/research', data);
 };
