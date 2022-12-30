3b4227167920e6f96fdf1e7263e59e945699a404
jinhwansuh <nick02190219@gmail.com>
2022-12-30 17:36:18 +0900

    feat: 데이터 값이 없을 경우, 분기처리

--- pages/result/index.tsx
+++ pages/result/index.tsx
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router';
 import { useCallback, useEffect, useState } from 'react';
+import { isEmpty } from 'lodash-es';
 import { useRecoilValue } from 'recoil';
 import { NextHead } from '~/components/common';
 import { Button } from '~/components/domains';
@@ -7,72 +8,88 @@ import { HouseData } from '~/types/house';
 import { calculateEstimateTime } from '~/utils/functions/house';
 import { getHouse } from '~/api/house';
 import { researchState } from '~/atoms/research';
+import { PAGE_ROUTE } from '~/constants';
 
 const ResearchResultPage = () => {
   const router = useRouter();
   const [houseState, setHouseState] = useState<HouseData>();
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const researchRecoilState = useRecoilValue(researchState);
   const [isError, setIsError] = useState(false);
-
-  // TODO: 로딩 spinner or something
-  // TODO: 전역상태에 값이 없을 때 (방어코드)
-  // TODO: 데이터가 없을 때
-  // TODO: 기본 값 제거
-  // TODO: 통신 에러가 발생했을 때
+  const [hasNoData, setHasNoData] = useState(false);
 
   const fetchHouse = useCallback(async () => {
     try {
       const { data } = await getHouse({
         jibunAddress: researchRecoilState.jibunAddress,
       });
-      if (data) {
+      if (isEmpty(data.data)) {
+        setHasNoData(() => true);
+      } else {
         setHouseState(() => ({
           ...data.data,
           estimateTime: calculateEstimateTime({
-            budget: researchRecoilState.cash
-              ? parseInt(researchRecoilState.cash, 10)
-              : 4000,
-            saving: researchRecoilState.saving
-              ? parseInt(researchRecoilState.saving, 10)
-              : 250,
-            rate: researchRecoilState.rate ? +researchRecoilState.rate : 5.5,
-            targetPrice: data.data.cost || 5000,
+            budget: +researchRecoilState.cash,
+            saving: +researchRecoilState.saving,
+            rate: +researchRecoilState.rate,
+            targetPrice: data.data.cost,
           }),
         }));
       }
+      setIsError(false);
     } catch {
+      // TODO: 통신 에러가 발생했을 때 처리
       setIsError(true);
     }
   }, [researchRecoilState]);
 
   useEffect(() => {
-    setIsLoading(() => true);
+    // TODO: 전역상태에 값이 없을 때 (방어코드)
     fetchHouse();
     setIsLoading(() => false);
   }, []);
 
   const handleButtonClick = () => {
-    router.push('/');
+    router.push(PAGE_ROUTE.HOME);
   };
 
-  if (!houseState || isLoading) return <div>데이터를 불러오고 있습니다...</div>;
-
-  if (!houseState?.danjiName && !isLoading) return <div>데이터가 없습니다</div>;
+  // TODO: 로딩 spinner or something
+  if (isLoading) return <div>데이터를 불러오고 있습니다...</div>;
 
   return (
     <>
       <NextHead title='결과' />
 
-      <div>{houseState.danjiName}</div>
-      <div>{houseState.cost}</div>
-      {houseState.estimateTime ? (
-        <div>
-          {houseState.estimateTime[0]}년 {houseState.estimateTime[1]}개월
-        </div>
+      {hasNoData ? (
+        <>
+          <div>주소를 정확하게 입력해주세요. </div>
+          <div>(ex, 압구정 현대아파트)</div>
+          <button onClick={() => router.push(PAGE_ROUTE.RESEARCH_SECOND)}>
+            다시 입력하러 가기
+          </button>
+        </>
       ) : (
-        <div>축하합니다 현재 자산으로 살수 있어요!</div>
+        <>
+          <div>{houseState?.danjiName}</div>
+          <div>{houseState?.cost}</div>
+          {houseState?.estimateTime ? (
+            <div>
+              {houseState.estimateTime[0]}년 {houseState.estimateTime[1]}개월
+            </div>
+          ) : (
+            <div>
+              축하합니다 {houseState?.danjiName}을 현재 자산으로 살수 있어요!
+            </div>
+          )}
+        </>
       )}
+      {isError && (
+        <>
+          <div>서버와 통신 오류입니다.</div>
+          <button onClick={fetchHouse}>데이터 다시 불러오기</button>
+        </>
+      )}
+
       <Button content='홈으로' handleButtonClick={handleButtonClick} />
     </>
   );
