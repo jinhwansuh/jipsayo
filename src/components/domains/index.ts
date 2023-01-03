6648c4284cac6c37c85a64251b1030845526cd5c
jinhwansuh <nick02190219@gmail.com>
2023-01-03 22:39:40 +0900

    feat: 간단한 Image carousel 컴포넌트 구현

--- src/components/domains/index.ts
+++ src/components/domains/index.ts
@@ -1,2 +1,3 @@
 export { default as EstimateInput } from './EstimateInput';
 export { default as Button } from './Button';
+export { default as ImageCarousel } from './ImageCarousel';
--- src/utils/house.ts
+++ src/utils/house.ts
@@ -28,3 +28,13 @@ export const mockHouseData = {
   createdDate: '2022-12-28T18:04:31',
   modifiedDate: '2022-12-28T18:04:31',
 };
+
+export const houseImageData = [
+  'https://images.unsplash.com/photo-1545324418-cc1a3fa10c00?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80',
+  'https://images.unsplash.com/photo-1580216643062-cf460548a66a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=327&q=80',
+  'https://images.unsplash.com/photo-1524235325756-750277bf9ac6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+  'https://images.unsplash.com/photo-1573987116136-8161883a27f1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=331&q=80',
+  'https://images.unsplash.com/photo-1505662482633-714f2c0e066e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=394&q=80',
+  'https://images.unsplash.com/photo-1500864685331-50f6398b3c51?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80',
+  'https://images.unsplash.com/photo-1534604554657-0581d533ef41?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=385&q=80',
+];
