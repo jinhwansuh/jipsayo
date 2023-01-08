767ad14674073284054f9e77a9c32ae68a438ff1
jinhwansuh <nick02190219@gmail.com>
2023-01-09 02:23:20 +0900

    refactor: 변경된 theme 경로 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import { AnimatePresence } from 'framer-motion';
 import { RecoilRoot } from 'recoil';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from '~/styles/GlobalStyle';
-import { theme } from '~/styles/theme';
+import theme from '~/styles/theme';
 import { DefaultLayout } from '~/layouts';
 
 export default function App({ Component, pageProps, router }: AppProps) {
