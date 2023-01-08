dc46ead1d1f3855bd782796a1b95c2acc69c4cbf
jinhwansuh <nick02190219@gmail.com>
2023-01-08 15:57:12 +0900

    refactor: DaumPostFrame 컴포넌트 적용

--- src/components/dynamicPage/DynamicResearch2/index.tsx
+++ src/components/dynamicPage/DynamicResearch2/index.tsx
@@ -4,6 +4,7 @@ import { motion } from 'framer-motion';
 import { useRecoilState } from 'recoil';
 import styled from 'styled-components';
 import Transitions from '~/layouts/Transitions';
+import { DaumPostFrame } from '~/components/domains';
 import { SearchAddressData, SearchResize } from '~/types/research';
 import { initialAddress } from '~/utils/house';
 import { postResearch } from '~/api/research';
@@ -137,19 +138,12 @@ const DynamicResearch2 = () => {
           )}
         </StyledAddressWrapper>
 
-        <StyledFrameContainer>
-          <StyledFrameWrapper
-            ref={searchFrameRef}
-            style={{ display: isOpen ? 'block' : 'none' }}
-          >
-            <StyledExitImg
-              src='//t1.daumcdn.net/postcode/resource/images/close.png'
-              id='btnFoldWrap'
-              onClick={frameCloseClick}
-              alt='접기 버튼'
-            />
-          </StyledFrameWrapper>
-        </StyledFrameContainer>
+        <DaumPostFrame
+          isOpen={isOpen}
+          searchFrameRef={searchFrameRef}
+          frameCloseClick={frameCloseClick}
+        />
+
         <div>
           {isComplete && (
             <StyleButtonWrapper>
@@ -188,30 +182,6 @@ const StyledSelectText = styled.div`
   text-align: center;
   font-size: 23px;
   height: 100%;
-`;
-
-const StyledFrameContainer = styled.div``;
-
-const StyledFrameWrapper = styled.div`
-  display: none;
-  border: 1px solid;
-  width: 500px;
-  height: 466px;
-  margin: 5px 0;
-  position: relative;
-  min-height: 400px;
-  max-height: 500px;
-  @media screen and (max-width: 400px) {
-    width: 90%;
-  }
-`;
-
-const StyledExitImg = styled.img`
-  cursor: pointer;
-  position: absolute;
-  right: 0px;
-  top: -1px;
-  z-index: 1;
 `;
 
 const StyleButtonWrapper = styled.div`
