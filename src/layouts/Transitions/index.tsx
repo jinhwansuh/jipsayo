0820b1c3b55c337034d605377ce2fe1402a3bb91
jinhwansuh <nick02190219@gmail.com>
2023-01-15 23:43:47 +0900

    design: 애니메이션 motion flex 사용하지 않도록 설정

--- src/layouts/Transitions/index.tsx
+++ src/layouts/Transitions/index.tsx
@@ -1,14 +1,15 @@
 import { ReactNode } from 'react';
 import { motion } from 'framer-motion';
-import styled from 'styled-components';
+import styled, { CSSProperties } from 'styled-components';
 
 interface Props {
   children: ReactNode;
+  style?: CSSProperties;
 }
 
-const Transitions = ({ children }: Props) => {
+const Transitions = ({ children, ...props }: Props) => {
   return (
-    <MotionWrapper
+    <StyledHorizontalMotion
       initial={{ x: 150, y: 0, opacity: 0 }}
       animate={{ x: 0, y: 0, opacity: 1 }}
       exit={{ x: -150, y: 0, opacity: 0 }}
@@ -17,15 +18,14 @@ const Transitions = ({ children }: Props) => {
         stiffness: 260,
         damping: 20,
       }}
+      {...props}
     >
       {children}
-    </MotionWrapper>
+    </StyledHorizontalMotion>
   );
 };
 
-const MotionWrapper = styled(motion.div)`
-  display: flex;
-  flex-direction: column;
+const StyledHorizontalMotion = styled(motion.div)`
   height: 100%;
 `;
 
