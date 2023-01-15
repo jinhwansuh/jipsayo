02f71e11400773047af96c8d67a680cd298e56b2
jinhwansuh <nick02190219@gmail.com>
2023-01-16 00:12:39 +0900

    feat: filterModal 적용

--- src/components/kakao/Map/Header.tsx
+++ src/components/kakao/Map/Header.tsx
@@ -1,12 +1,51 @@
+import { memo, useCallback, useState } from 'react';
 import styled from 'styled-components';
+import { FilterModal } from '~/components/domains';
 
 const MapHeader = () => {
-  return <StyledHeader>header</StyledHeader>;
+  const [filterModalOpen, setFilterModalOpen] = useState(false);
+
+  const handleFilterClick = useCallback(() => {
+    setFilterModalOpen(true);
+  }, []);
+
+  return (
+    <>
+      <StyledContainer>
+        <StyledWrapper>
+          <StyledHeader>
+            <div>filter button</div>
+            <button onClick={handleFilterClick}>filter open</button>
+          </StyledHeader>
+        </StyledWrapper>
+      </StyledContainer>
+
+      <FilterModal
+        filterModalOpen={filterModalOpen}
+        setFilterModalOpen={setFilterModalOpen}
+      />
+    </>
+  );
 };
 
-const StyledHeader = styled.div`
+const StyledContainer = styled.div`
+  position: relative;
   height: 86px;
-  background: #ffffff;
+  background: #fff;
+  padding-bottom: 16px;
+  box-shadow: 0 8px 6px -8px black;
+  z-index: 2;
 `;
 
-export default MapHeader;
+const StyledWrapper = styled.div`
+  padding: 14px 24px 0 24px;
+`;
+
+const StyledHeader = styled.div`
+  min-height: 56px;
+  min-width: 285px;
+  background: #f7f7f7;
+  border-radius: 1000px;
+`;
+
+export default memo(MapHeader);
