fb5e2be87f0267e3ba02ad073d9091ace629fe63
jinhwansuh <nick02190219@gmail.com>
2023-01-18 02:41:17 +0900

    refactor: 카카오 지도 독자적인 상태를 관리하도록 변경

--- src/components/kakao/Map/index.tsx
+++ src/components/kakao/Map/index.tsx
@@ -1,21 +1,51 @@
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { isEmpty } from 'lodash-es';
+import { useRecoilValue } from 'recoil';
 import styled from 'styled-components';
 import { DaumPostFrame } from '~/components/domains';
+import { getHouse } from '~/api/house';
+import { houseState } from '~/atoms/house';
 import { useDaumPost } from '~/hooks';
 import MapHeader from './Header';
 import KakaoMap from './Map';
 
-interface Props {
-  latitude: number;
-  longitude: number;
-}
-
-const KakaoMapContainer = ({ latitude, longitude }: Props) => {
+const KakaoMapContainer = () => {
+  const houseRecoilState = useRecoilValue(houseState);
+  const [locationState, setLocationState] = useState({
+    latitude: houseRecoilState.latitude || 33.45,
+    longitude: houseRecoilState.longitude || 126.57,
+  });
   const searchFrameRef = useRef(null);
   const { isOpen, frameOpenClick, frameCloseClick, isComplete, addressState } =
     useDaumPost({
       searchFrameRef,
     });
+
+  const handleFetchHouseData = async () => {
+    const { roadAddress, buildingName } = addressState;
+    const { data } = await getHouse({
+      roadAddress: roadAddress,
+      danjiName: buildingName,
+    });
+
+    if (isEmpty(data.data)) {
+      // noData
+    } else {
+      setLocationState({
+        latitude: +data.data.longitude,
+        longitude: +data.data.latitude,
+      });
+
+      // latitude: data.data.latitude,
+      //   longitude: data.data.longitude,
+    }
+  };
+
+  useEffect(() => {
+    if (isComplete) {
+      handleFetchHouseData();
+    }
+  }, [isComplete]);
 
   return (
     <StyledContainer>
@@ -30,7 +60,7 @@ const KakaoMapContainer = ({ latitude, longitude }: Props) => {
         frameCloseClick={frameCloseClick}
         position={'absolute'}
       />
-      <KakaoMap latitude={latitude} longitude={longitude} />
+      <KakaoMap location={locationState} />
     </StyledContainer>
   );
 };
