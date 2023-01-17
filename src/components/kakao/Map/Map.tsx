8bb0a4370f474f1303eb3f186914a48e8314344d
jinhwansuh <nick02190219@gmail.com>
2023-01-18 02:44:23 +0900

    feat: 새로운 주소를 입력받았을 때, 지도 위치변경 기능 구현

--- src/components/kakao/Map/Map.tsx
+++ src/components/kakao/Map/Map.tsx
@@ -1,25 +1,32 @@
-import { memo, useEffect, useRef, useState } from 'react';
+import { useRouter } from 'next/router';
+import { useEffect, useRef, useState } from 'react';
+import { isEmpty } from 'lodash-es';
 import styled from 'styled-components';
 
 interface Props {
-  latitude: number;
-  longitude: number;
+  location: {
+    latitude: number;
+    longitude: number;
+  };
 }
 
-const KakaoMap = ({ latitude, longitude }: Props) => {
+const KakaoMap = ({ location }: Props) => {
+  const router = useRouter();
   const [kakaoMap, setKakaoMap] = useState<any>(null);
   const [mapOptions, setMapOptions] = useState({
-    latlng: {
-      latitude,
-      longitude,
-    },
+    location,
     level: 5,
   });
   const kakaoMapRef = useRef<HTMLDivElement>(null);
+  const query = router.query;
 
   useEffect(() => {
     const kakaomapCurrent = kakaoMapRef.current;
   }, []);
+
+  useEffect(() => {
+    if (isEmpty(query)) return;
+  }, [query]);
 
   useEffect(() => {
     if (kakaoMapRef && kakaoMapRef.current) {
@@ -27,8 +34,8 @@ const KakaoMap = ({ latitude, longitude }: Props) => {
         const container = kakaoMapRef.current;
         const options = {
           center: new kakao.maps.LatLng(
-            mapOptions.latlng.latitude,
-            mapOptions.latlng.longitude,
+            mapOptions.location.latitude,
+            mapOptions.location.longitude,
           ),
           level: mapOptions.level,
         };
@@ -37,52 +44,34 @@ const KakaoMap = ({ latitude, longitude }: Props) => {
         setKakaoMap(map);
 
         // 확대 축소 true, false
-        map.setZoomable(false);
-
-        const markerPosition = new kakao.maps.LatLng(
-          mapOptions.latlng.latitude,
-          mapOptions.latlng.longitude,
-        );
-        const marker = new kakao.maps.Marker({
-          position: markerPosition,
-        });
-        marker.setMap(map);
+        // map.setZoomable(false);
 
         // 이동 true, false
-        map.setDraggable(true);
+        // map.setDraggable(true);
         kakao.maps.event.addListener(map, 'dragend', function () {
-          console.log('드래그 이동');
+          // console.log('드래그 이동');
         });
       });
     }
   }, [kakaoMapRef]);
 
-  // useEffect(() => {
-  //   if (kakaoMap === null) {
-  //     return;
-  //   }
+  useEffect(() => {
+    if (kakaoMap === null) {
+      return;
+    }
+    setMapOptions((prev) => ({ ...prev, location }));
 
-  //   const positions = markerData.map((pos) => new kakao.maps.LatLng(...pos));
-
-  //   setMarkers((markers) => {
-  //     // clear prev markers
-  //     markers.forEach((marker) => marker.setMap(null));
-
-  //     // assign new markers
-  //     return positions.map(
-  //       (position) => new kakao.maps.Marker({ map: kakaoMap, position }),
-  //     );
-  //   });
-
-  //   if (positions.length > 0) {
-  //     const bounds = positions.reduce(
-  //       (bounds, latlng) => bounds.extend(latlng),
-  //       new kakao.maps.LatLngBounds(),
-  //     );
-
-  //     kakaoMap.setBounds(bounds);
-  //   }
-  // }, [kakaoMap, markerData]);
+    const markerPosition = new kakao.maps.LatLng(
+      location.latitude,
+      location.longitude,
+    );
+    const marker = new kakao.maps.Marker({
+      position: markerPosition,
+    });
+    kakaoMap.relayout();
+    kakaoMap.setCenter(markerPosition);
+    marker.setMap(kakaoMap);
+  }, [kakaoMap, location]);
 
   return (
     <Container>
@@ -101,4 +90,4 @@ const StyledMap = styled.div`
   height: 100%;
 `;
 
-export default memo(KakaoMap);
+export default KakaoMap;
