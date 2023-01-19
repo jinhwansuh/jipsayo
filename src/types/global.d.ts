28d8d8de15a7b6ff7e42b35012893d408a2e8e08
jinhwansuh <nick02190219@gmail.com>
2023-01-19 16:14:57 +0900

    refactor: global type 하나로 병합 (kakao, daum)

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -2,8 +2,8 @@ declare global {
   interface Window {
     gtag: (param1: string, param2: string, param3: object) => void;
   }
+  const kakao: any;
+  const daum: any;
 }
-declare const daum: any;
-declare const kakao: any;
 
 export {};
