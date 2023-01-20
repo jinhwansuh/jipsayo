17ed77b598db593fffb76dcf0ea4695c14190662
jinhwansuh <nick02190219@gmail.com>
2023-01-21 02:12:02 +0900

    refactor: 전역상태에서 데이터를 관리하던 것을 컴포넌트에서 관리하도록 변경

--- src/components/kakao/Map/Map.tsx
+++ src/components/kakao/Map/Map.tsx
@@ -1,22 +1,20 @@
 import { useRouter } from 'next/router';
 import { useEffect, useRef, useState } from 'react';
 import { isEmpty } from 'lodash-es';
-import { useRecoilValue } from 'recoil';
 import styled from 'styled-components';
-import { LocationState } from '~/types/house';
-import { filteredHouseSelector } from '~/atoms/house';
+import { FetchFilteredHouseDate, LocationState } from '~/types/house';
 
 interface Props {
   location: LocationState;
+  filteredHouseState: FetchFilteredHouseDate[];
 }
 
-const KakaoMap = ({ location }: Props) => {
+const KakaoMap = ({ location, filteredHouseState }: Props) => {
   const router = useRouter();
   const kakaoMapRef = useRef<HTMLDivElement>(null);
-  const filteredHouseState = useRecoilValue(filteredHouseSelector);
   const [kakaoMap, setKakaoMap] = useState<any>(null);
   const [, setCenterMarker] = useState<any>();
-  const [, setMarkers] = useState<any>([]);
+  const [, setMarkers] = useState<any[]>([]);
   const query = router.query;
 
   useEffect(() => {
@@ -27,6 +25,7 @@ const KakaoMap = ({ location }: Props) => {
     if (isEmpty(query)) return;
   }, [query]);
 
+  // init map 정의
   useEffect(() => {
     if (kakaoMapRef && kakaoMapRef.current) {
       kakao.maps.load(() => {
@@ -41,12 +40,8 @@ const KakaoMap = ({ location }: Props) => {
 
         // 확대 축소 true, false
         // map.setZoomable(false);
-
         // 이동 true, false
         // map.setDraggable(true);
-        kakao.maps.event.addListener(map, 'dragend', function () {
-          // console.log('드래그 이동');
-        });
       });
     }
   }, [kakaoMapRef]);
@@ -71,8 +66,6 @@ const KakaoMap = ({ location }: Props) => {
     kakaoMap.setCenter(markerPosition);
   }, [kakaoMap, location]);
 
-  console.log(location);
-
   // 필터링된 데이터가 바뀌었을 때,
   useEffect(() => {
     if (kakaoMap === null) {
@@ -84,10 +77,7 @@ const KakaoMap = ({ location }: Props) => {
     );
 
     setMarkers((markers: any) => {
-      // clear prev markers
       markers.forEach((marker: any) => marker.setMap(null));
-
-      // assign new markers
       return positions.map(
         (position) => new kakao.maps.Marker({ map: kakaoMap, position }),
       );
