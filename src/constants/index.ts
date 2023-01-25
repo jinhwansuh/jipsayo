9b15c8fb2f5a4a84b8296a38493da0a769b0a2e8
jinhwansuh <nick02190219@gmail.com>
2023-01-25 19:09:06 +0900

    feat: kakao지도 기준 이미지 src 추가

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -15,4 +15,6 @@ export const PAGE_ROUTE = {
 export const KAKAO_URL = {
   POSTCODE: '//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js',
   MAP: `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_API_KEY}&libraries=services&autoload=false`,
+  CENTER_IMAGE:
+    'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png',
 } as const;
