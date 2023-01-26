3b07a435e30f9e30e901f865bad75551ac8dd45c
jinhwansuh <nick02190219@gmail.com>
2023-01-26 15:11:25 +0900

    feat: remixIcon 색상 변경할 수 있도록 추가

--- src/components/common/Remixicon/index.tsx
+++ src/components/common/Remixicon/index.tsx
@@ -1,10 +1,18 @@
+// https://remixicon.com/
+
 interface Props {
   iconName: string;
   size: string;
+  color?: string;
 }
 
-const Remixicon = ({ iconName, size }: Props) => {
-  return <i className={`${iconName}`} style={{ fontSize: `${size}` }} />;
+const Remixicon = ({ iconName, size, color }: Props) => {
+  return (
+    <i
+      className={`${iconName}`}
+      style={{ fontSize: `${size}`, color: color || 'black' }}
+    />
+  );
 };
 
 export default Remixicon;
