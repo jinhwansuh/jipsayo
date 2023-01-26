3dc4258837ea4b4ba9c6ca8bd5b9deeff04c3746
jinhwansuh <nick02190219@gmail.com>
2023-01-27 04:22:51 +0900

    feat: 디자인 변경이 안되는 infowindow에서 customoverlay로 변경

--- src/components/kakao/Map/Map.tsx
+++ src/components/kakao/Map/Map.tsx
@@ -2,6 +2,7 @@ import { useRouter } from 'next/router';
 import { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 import { FetchFilteredHouseDate, LocationState } from '~/types/house';
+import { makeOverlayContent } from '~/utils/functions/kakao';
 import { KAKAO_URL } from '~/constants';
 
 interface Props {
@@ -108,29 +109,32 @@ const KakaoMap = ({ locationState, filteredHouseState }: Props) => {
       const position = new kakao.maps.LatLng(state.latitude, state.longitude);
       const marker = new kakao.maps.Marker({
         map: kakaoMap,
-        position,
+        position: position,
         clickable: true,
       });
 
       setMarkers((prev) => [...prev, marker]);
 
-      const iwContent =
-        `<div style="padding:5px;">` +
-        `<div>${state.danjiName}</div>` +
-        `<div>${state.jibunAddress}</div>` +
-        `</div>`;
-      const infowindow = new kakao.maps.InfoWindow({
-        content: iwContent,
-        removable: true,
+      const overlay = new kakao.maps.CustomOverlay({
+        position: marker.getPosition(),
+        clickable: true,
+      });
+
+      const content = makeOverlayContent({
+        customOverlay: overlay,
+        danjiName: state.danjiName,
+        jibunAddress: state.jibunAddress,
       });
 
       kakao.maps.event.addListener(marker, 'click', function () {
-        setClickedMarker((prevInfoWindow: any) => {
+        setClickedMarker((prevInfoOverlay: any) => {
           // 이전 open된 값이 있다면 close
-          if (prevInfoWindow) prevInfoWindow.close();
-          infowindow.open(kakaoMap, marker);
-          return infowindow;
+          if (prevInfoOverlay) prevInfoOverlay.setMap(null);
+          return overlay;
         });
+
+        overlay.setContent(content);
+        overlay.setMap(kakaoMap);
       });
 
       return position;
