afbdf7a90ecbfad213993aefa3d4b1c3a236132a
jinhwansuh <nick02190219@gmail.com>
2023-01-30 02:31:02 +0900

    refactor: 타입 변수명 변경

--- src/types/research.ts
+++ src/types/research.ts
@@ -59,10 +59,11 @@ export interface PostResearchRequest {
 export interface PostResearchResponse {
   id: number;
 }
+export type PrefetchedHouse = {
+  roadAddress: string;
+  danjiName: string;
+};
 
-export interface prefetchedHouse {
-  data: {
-    roadAddress: string;
-    danjiName: string;
-  }[];
+export interface PrefetchedHouseResponse {
+  data: PrefetchedHouse[];
 }
