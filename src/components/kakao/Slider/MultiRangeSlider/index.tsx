a9d6f86ed7ee4914f975aa8cd5208da4ba0a030d
jinhwansuh <nick02190219@gmail.com>
2023-01-29 18:30:13 +0900

    refactor: 공통으로 사용되는 Slider style 분리, 적용

--- src/components/kakao/Slider/MultiRangeSlider/index.tsx
+++ src/components/kakao/Slider/MultiRangeSlider/index.tsx
@@ -2,6 +2,14 @@ import { ChangeEvent, memo, useCallback, useState } from 'react';
 import styled from 'styled-components';
 import { FilterPriceState } from '~/types/house';
 import { calculateCostToWon } from '~/utils/functions/house';
+import {
+  StyledContainer,
+  StyledInput,
+  StyledInputWrapper,
+  StyledRange,
+  StyledRangeWrapper,
+  StyledTrack,
+} from '../Slider.styled';
 
 // left보다 translateX를 사용하는 것이 더 부드럽다. (모바일 환경)
 // https://www.paulirish.com/2012/why-moving-elements-with-translate-is-better-than-posabs-topleft/
@@ -103,63 +111,7 @@ const MultiRangeSlider = ({
   );
 };
 
-const StyledContainer = styled.div`
-  position: relative;
-  margin-top: 20px;
-`;
-
-const StyledRangeWrapper = styled.div`
-  position: relative;
-  height: 10px;
-`;
-
-const StyledRange = styled.div`
-  background: #e5e8eb;
-  width: 100%;
-  height: 100%;
-  border-radius: ${(props) => props.theme.input.track_border_radius};
-`;
-
-const StyledTrack = styled.div`
-  position: absolute;
-  height: 100%;
-  background: ${(props) => props.theme.color.blue_default};
-  will-change: left, right;
-  border-radius: 5px;
-`;
-
-const StyledInputWrapper = styled.div`
-  position: relative;
-  top: -4px;
-`;
-
-const StyledMaxInput = styled.input`
-  position: absolute;
-  -webkit-appearance: none;
-  height: 100%;
-  width: 100%;
-  background: transparent;
-  padding: 0;
-  pointer-events: none;
-  cursor: pointer;
-  left: -0.5px;
-
-  &::-webkit-slider-thumb {
-    -webkit-appearance: none;
-    height: 18px;
-    width: 18px;
-    border-radius: 100%;
-    background-color: #0f2957;
-    box-shadow: 0 0 0 1px rgba(0, 27, 55, 0.1),
-      0 8px 8px 0 rgba(0, 29, 58, 0.18), 0 2px 3px 0 rgba(0, 29, 58, 0.18);
-    border: none;
-    pointer-events: auto;
-  }
-
-  &:focus {
-    outline: none;
-  }
-`;
+const StyledMaxInput = styled(StyledInput)``;
 
 const StyledMinInput = styled(StyledMaxInput)``;
 
