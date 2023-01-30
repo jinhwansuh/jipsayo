a6fedc4d147d83b5f213d2a0363ccbc3e1ed3fe9
jinhwansuh <nick02190219@gmail.com>
2023-01-30 20:43:13 +0900

    feat: header컴포넌트 backButton 추가

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -1,16 +1,33 @@
 import { useRouter } from 'next/router';
+import { useCallback } from 'react';
 import styled from 'styled-components';
+import { ValueOf } from '~/types/helper';
 import { PAGE_ROUTE } from '~/constants';
+import Remixicon from '../Remixicon';
 
-const Header = () => {
+interface Props {
+  backButton: boolean;
+  pageTo?: ValueOf<typeof PAGE_ROUTE>;
+}
+
+const Header = ({ backButton, pageTo }: Props) => {
   const router = useRouter();
 
-  const handleHomeClick = () => {
+  const handleBackClick = useCallback(() => {
+    if (pageTo) router.push(pageTo);
+  }, [pageTo]);
+
+  const handleHomeClick = useCallback(() => {
     router.push(PAGE_ROUTE.HOME);
-  };
+  }, []);
 
   return (
     <Container>
+      {backButton && (
+        <StyledIcon onClick={handleBackClick}>
+          <Remixicon iconName='ri-arrow-left-s-line' size='30px' />
+        </StyledIcon>
+      )}
       <HeaderText onClick={handleHomeClick}>집사요</HeaderText>
     </Container>
   );
@@ -18,11 +35,22 @@ const Header = () => {
 
 const Container = styled.header`
   z-index: 5000;
+  max-width: ${(props) => props.theme.width.default_global_width};
   min-height: ${(props) => props.theme.height.header_height};
   background-color: ${(props) => props.theme.color.default_header};
   display: flex;
   justify-content: center;
   align-items: center;
+  position: relative;
+  margin: 0 auto;
+`;
+
+const StyledIcon = styled.span`
+  position: absolute;
+  left: 10px;
+  top: 25px;
+  transform: translate(0, -40%);
+  cursor: pointer;
 `;
 
 const HeaderText = styled.div`
