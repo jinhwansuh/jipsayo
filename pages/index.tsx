2b3e05e219aa38ba0e911d2f6c38a9bfeaf75527
jinhwansuh <nick02190219@gmail.com>
2023-01-30 20:58:39 +0900

    refactor: back button이 있는 header와 기본 layout을 담는 Container 적용

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,42 +1,46 @@
 import styled from 'styled-components';
-import { NextImage } from '~/components/common';
+import { Header, NextImage } from '~/components/common';
 import { Content } from '~/components/home';
 import { PAGE_ROUTE } from '~/constants';
+import { Container } from '~/layouts';
 
 export default function Home() {
   return (
     <>
-      <StyledTitleWrapper>
-        <StyledTitle>집사기 위한 최고의 서비스</StyledTitle>
+      <Header backButton={false} />
+      <Container>
+        <StyledTitleWrapper>
+          <StyledTitle>집사기 위한 최고의 서비스</StyledTitle>
 
-        <NextImage
-          imageSrc={'/image/jipsayo.png'}
-          alt='logo'
-          width={'120px'}
-          height={'70px'}
-          priority={true}
-        />
-      </StyledTitleWrapper>
+          <NextImage
+            imageSrc={'/image/jipsayo.png'}
+            alt='logo'
+            width={'120px'}
+            height={'70px'}
+            priority={true}
+          />
+        </StyledTitleWrapper>
 
-      <StyledContentWrapper>
-        <Content
-          title='예상 기간을 조회하세요'
-          pageTo={PAGE_ROUTE.RESEARCH_FIRST}
-          imageSrc={'/image/result.png'}
-          imagePriority={true}
-        >
-          내 자산과 한 달 저축 금액을 입력하여 원하는 집을 언제쯤 살 수 있을지
-          테스트해 보세요!
-        </Content>
+        <StyledContentWrapper>
+          <Content
+            title='예상 기간을 조회하세요'
+            pageTo={PAGE_ROUTE.RESEARCH_FIRST}
+            imageSrc={'/image/result.png'}
+            imagePriority={true}
+          >
+            내 자산과 한 달 저축 금액을 입력하여 원하는 집을 언제쯤 살 수 있을지
+            테스트해 보세요!
+          </Content>
 
-        <Content
-          title='거리를 계산하세요'
-          pageTo={PAGE_ROUTE.MAP}
-          imageSrc={'/image/map.png'}
-        >
-          내 직장 주변에 조건에 맞는 어떤 집이 있을까 찾아보세요!
-        </Content>
-      </StyledContentWrapper>
+          <Content
+            title='거리를 계산하세요'
+            pageTo={PAGE_ROUTE.MAP}
+            imageSrc={'/image/map.png'}
+          >
+            내 직장 주변에 조건에 맞는 어떤 집이 있을까 찾아보세요!
+          </Content>
+        </StyledContentWrapper>
+      </Container>
     </>
   );
 }
--- pages/result/index.tsx
+++ pages/result/index.tsx
@@ -3,10 +3,11 @@ import { useCallback, useEffect, useState } from 'react';
 import { useRecoilValue } from 'recoil';
 import styled from 'styled-components';
 import { ErrorNoResultData } from '~/components/Error';
-import { NextHead } from '~/components/common';
+import { Header, NextHead } from '~/components/common';
 import { HasHouseData } from '~/components/result';
 import { houseStateSelector } from '~/atoms/house';
 import { PAGE_ROUTE } from '~/constants';
+import { Container } from '~/layouts';
 
 const ResearchResultPage = () => {
   const router = useRouter();
@@ -28,23 +29,28 @@ const ResearchResultPage = () => {
   return (
     <>
       <NextHead title='결과' />
-      <StyledContainer>
-        {houseRecoilState.estimateTime === false ? (
-          <div>
-            축하합니다!!{' '}
-            <StyledMarkText>{houseRecoilState.danjiName}</StyledMarkText> 을
-            현재 자산으로 살수 있어요!
-          </div>
-        ) : houseRecoilState.cost ? (
-          <HasHouseData
-            houseState={houseRecoilState}
-            handleButtonClick={handlePushPrevPage}
-          />
-        ) : null}
 
-        {/* 데이터가 없을 때 */}
-        {hasNoData && <ErrorNoResultData />}
-      </StyledContainer>
+      <Header backButton={true} pageTo={PAGE_ROUTE.RESEARCH_SECOND} />
+
+      <Container>
+        <StyledContainer>
+          {houseRecoilState.estimateTime === false ? (
+            <div>
+              축하합니다!!{' '}
+              <StyledMarkText>{houseRecoilState.danjiName}</StyledMarkText> 을
+              현재 자산으로 살수 있어요!
+            </div>
+          ) : houseRecoilState.cost ? (
+            <HasHouseData
+              houseState={houseRecoilState}
+              handleButtonClick={handlePushPrevPage}
+            />
+          ) : null}
+
+          {/* 데이터가 없을 때 */}
+          {hasNoData && <ErrorNoResultData />}
+        </StyledContainer>
+      </Container>
     </>
   );
 };
