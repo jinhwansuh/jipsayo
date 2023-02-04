2353ee751d6bfc41864dfadf565c1f4c373cf80e
jinhwansuh <nick02190219@gmail.com>
2023-02-04 13:54:52 +0900

    refactor: input 오류 문구 변경

--- src/components/Error/ErrorNoInputValue.tsx
+++ src/components/Error/ErrorNoInputValue.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const ErrorNoInputValue = () => {
   return (
     <ErrorContainer>
-      <ErrorMessage>모든 항목을 입력해주세요</ErrorMessage>
+      <ErrorMessage>모든 항목을 정확히 입력해주세요</ErrorMessage>
     </ErrorContainer>
   );
 };
